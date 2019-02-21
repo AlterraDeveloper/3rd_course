770e097ffdcc4c2c9d779db497c347fdb120b1af
unknown <jkiselev2411@gmail.com>
2019-02-21 11:54:31 +0600

    almost solve lab1

--- computer_model/lab1/script.m
+++ computer_model/lab1/script.m
@@ -3,4 +3,13 @@ t0 = 1;
 tf = 2;
 y0 = [14 16];
 [t ,y] = ode45('vdpol',[t0 tf],y0);
+[t,y]
 plot(t,y),title('Van der Pol equation time history')
+
+
+% clc
+% 
+% A = [1 log(8/7) ; 0 -450/224]
+% B = [14 16]
+% 
+% B*inv(A)

e6825e6697a472aed413b534fc9ca2e1cd7e75b7
alterradeveloper <jkiselev2411@gmail.com>
2019-02-28 07:40:11 +0600

    solve equation of the second order

--- computer_model/lab1/script.m
+++ computer_model/lab1/script.m
@@ -3,13 +3,13 @@ t0 = 1;
 tf = 2;
 y0 = [14 16];
 [t ,y] = ode45('vdpol',[t0 tf],y0);
-[t,y]
+%[t,y]
 plot(t,y),title('Van der Pol equation time history')
 
+clc
+A = [1 log(8/7) ; 0 -450/224]
+B = [14 16] 
+[C1 C2] = B*inv(A)
 
-% clc
-% 
-% A = [1 log(8/7) ; 0 -450/224]
-% B = [14 16]
-% 
-% B*inv(A)
+FtCC = C1 + C2*log((15*t+1)/(15*t-1))
+plot(t,FtCC)

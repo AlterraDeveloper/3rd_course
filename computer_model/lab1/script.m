e36e8e7b8a9584a411226c2daf9cc403a132f79b
jkiselev <jkiselev2411@gmail.com>
2019-02-28 11:52:41 +0600

    draw plot

--- computer_model/lab1/script.m
+++ computer_model/lab1/script.m
@@ -3,13 +3,18 @@ t0 = 1;
 tf = 2;
 y0 = [14 16];
 [t ,y] = ode45('vdpol',[t0 tf],y0);
-%[t,y]
+[t,y];
 plot(t,y),title('Van der Pol equation time history')
 
-clc
-A = [1 log(8/7) ; 0 -450/224]
-B = [14 16] 
-[C1 C2] = B*inv(A)
+A = [1 log(8/7) ; 0 -450/224];
+B = [14;16] ;
+C = A\B;
 
-FtCC = C1 + C2*log((15*t+1)/(15*t-1))
-plot(t,FtCC)
+FtCC = zeros(numel(t),1);
+c = 1;
+for i = t.'
+ FtCC(c,1) = C(1) + C(2)*log((15*i+1)/(15*i-1));
+ c = c+1;
+end
+FtCC;
+plot(t,FtCC),title('My solution')

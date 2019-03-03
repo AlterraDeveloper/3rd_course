7024c39e1c6d0f20b5e48e66c14e4054739d71d1
Eugene Kiselev <jkiselev2411@gmail.com>
2019-03-04 01:33:25 +0600

    graphics lab1 almost done

--- graphics/lab1/GalaxyMaker.py
+++ graphics/lab1/GalaxyMaker.py
@@ -1,10 +1,13 @@
-from math import sin,cos,exp,pi,trunc
+from math import sin,cos,exp,pi,trunc,sqrt,hypot
 from random import random
 
 class Star():
     def __init__(self,x,y):
         self.x = x
         self.y = y
+    
+    def calculate_distance(self,x,y):
+        return int(hypot(self.x - x,self.y-y))
     
 class GalaxyMaker():
 
@@ -19,35 +22,58 @@ class GalaxyMaker():
         self._rukav_h = self.height
         self._rukav_l = width/10
         
-    def _functional(self,d,r,galaxy_type):
-        if galaxy_type == 0:
-            pass
-        elif galaxy_type == 1: 
-            pass
-        elif galaxy_type == 2:
-            return exp(-(d**2.5)/(r**2))
+    def _functional_for_mindal(self,x,y):
+        a , b = self.width/10 , self.height/5
+        p = (x-self._x_center)**2/a**2+(y-self._y_center)**2/b**2
+        return exp(-p**1.2)
 
+    def make_mindal_galaxy(self):
+        self.stars.clear()
+        for x in range(self.width):
+            for y in range(self.height):
+                if self._functional_for_mindal(x,y) >= random():
+                    self.stars.append(Star(x,y))
+        return self.stars
+
+    def _functional_for_ellipse(self,x,y):
+        point = Star(x,y)
+        return exp((-point.calculate_distance(self._x_center,self._y_center)**2.65)/self._rukav_h**2)
     
-    def _g(self,x,y):
-        galaxy_type = 2 # spiral galaxy id
-        return self._functional((x)**2,self._rukav_l**2,galaxy_type)*self._functional((y)**2,self._rukav_h**2,galaxy_type)
+    def make_ellipse_galaxy(self):
+        self.stars.clear()
+        for x in range(self.width):
+            for y in range(self.height):
+                if self._functional_for_ellipse(x,y) >= random():
+                    self.stars.append(Star(x,y))
+        return self.stars
+    
+    def _g(self,d,r):
+        d *= 0.15
+        return exp(-(d**3)/(r**2))
+    
+    def _functional_for_spiral(self,x,y):
+        return self._g((x)**2,self._rukav_l**2)*self._g((y)**2,self._rukav_h**2)     
     
     def _rot(self,x,y,a):
         return int(x*cos(a) + y*sin(a)),int(-x*sin(a) + y*cos(a))
     
     def make_spiral_galaxy(self,arms,alpha):
+        self.stars.clear()
         radius = min(self.width,self.height)
         radius = trunc(((radius**2+radius**2)**0.5)/2)
         b = 0.2 * alpha * pi * radius*2 #TODO bind value of slider to second arg
         for x in range(-radius,radius):
             for y in range(-radius,radius):
-                if self._g(x,y) >= random():
+                if self._functional_for_spiral(x,y) >= random():
                     d = (x*x + y*y)**0.5
                     if d != 0:
                         new_x,new_y = self._rot(x,y,b/d)
                         self.stars.append(Star(new_x+self._x_center,new_y+self._y_center))
-                        new_x,new_y = self._rot(new_x,new_y,pi / arms)
-                        self.stars.append(Star(new_x+self._x_center,new_y+self._y_center))
+        rotation_angle = 2*pi / arms
+        for star in self.stars:
+            for i in range(arms):
+                new_x,new_y = self._rot(x,y,rotation_angle*i)
+                self.stars.append(Star(new_x+self._x_center,new_y+self._y_center))
         return self.stars
 
 if __name__ == "__main__" : pass
--- graphics/lab1/MainWindow.py
+++ graphics/lab1/MainWindow.py
@@ -1,3 +1,4 @@
+import math
 import tkinter as tk
 from tkinter.ttk import *
 from PIL import ImageTk,Image
@@ -7,34 +8,50 @@ class MainWindow(tk.Frame):
     def __init__(self,master=None):
         super().__init__(master)
         self.master = master
-        self.pack()
+        # self.pack()
+        self.galaxy_type = tk.IntVar()
         self.create_widgets()
+        self.canvas_center = (300,300)
     
     def create_widgets(self):
-        self.galaxy_radiobtn_group = list()
-        self.galaxy_radiobtn_group.append(tk.Radiobutton(self.master,text='Эллиптическая',value = 0).pack(side=tk.TOP))
-        self.galaxy_radiobtn_group .append(tk.Radiobutton(self.master,text='Миндалевидная',value = 1).pack(side=tk.TOP))
-        self.galaxy_radiobtn_group .append(tk.Radiobutton(self.master,text='Спиральная',value = 2).pack(side=tk.TOP))
-
+        self.radiobtn_ellipse_galaxy = tk.Radiobutton(self.master,text='Эллиптическая',value = 0,command = self.draw_stars,variable = self.galaxy_type)
+        self.radiobtn_ellipse_galaxy.grid(row = 0,column = 0)
+ 
+        self.radiobtn_mindal_galaxy = tk.Radiobutton(self.master,text='Миндалевидная',value = 1,command = self.draw_stars,variable = self.galaxy_type)
+        self.radiobtn_mindal_galaxy.grid(row = 0,column = 1)
+ 
+        self.radiobtn_spiral_galaxy = tk.Radiobutton(self.master,text='Спиральная',value = 2,command = self.draw_stars,variable = self.galaxy_type)
+        self.radiobtn_spiral_galaxy.grid(row = 0,column = 2)
+  
         self.galaxy_area = tk.Canvas(self.master,width = 600 ,height = 600,bg = 'black')
-        self.galaxy_area.pack()
-        img = Image.open(r"/home/altersoft/alterra/3_rd/3rd_course/graphics/lab1/space_bg.jpg")
-        bg_image = ImageTk.PhotoImage(img)
-        self.galaxy_area.create_image(0,0,image = bg_image)
+        self.galaxy_area.grid(row = 1,column = 0,columnspan = 3)
+  
 
-        
+    def calculate_color_of_star(self,distance):
+        r = hex((255+distance%255)%255) 
+        g = hex(0) 
+        b = hex(255 - distance%255)
+        return '#' + r[r.index('x')+1:][0] + g[g.index('x')+1:][0] + b[b.index('x')+1:][0]
+
    
-    def do_smth(self):
-        stars = GalaxyMaker(600,600).make_spiral_galaxy(3,2)
+    def draw_stars(self):
+        self.galaxy_area.delete('all')
+        if self.galaxy_type.get() == 0:
+            stars = GalaxyMaker(600,600).make_ellipse_galaxy()   
+        if self.galaxy_type.get() == 1:
+            stars = GalaxyMaker(600,600).make_mindal_galaxy()        
+        if self.galaxy_type.get() == 2:
+            stars = GalaxyMaker(600,600).make_spiral_galaxy(3,0)           
         for star in stars:
-            self.galaxy_area.create_line(star.x,star.y,star.x+1,star.y,fill='red')
+            distance_to_the_center = star.calculate_distance(self.canvas_center[0],self.canvas_center[1])
+            color_string = self.calculate_color_of_star(distance_to_the_center)
+            self.galaxy_area.create_line(star.x,star.y,star.x+1,star.y,fill=color_string)
 
         
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry("600x600+200+100")
+    root.geometry("600x650+200+50")
     root.title("Lab 1")
     root.resizable(False,False)
     app = MainWindow(master=root)
-    app.do_smth()
     app.mainloop()

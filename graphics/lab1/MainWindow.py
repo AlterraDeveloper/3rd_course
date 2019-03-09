e97206a4b15ffd561284cfe01f10d75a85679b99
Eugene Kiselev <jkiselev2411@gmail.com>
2019-03-09 14:15:31 +0600

    commit

--- graphics/lab1/MainWindow.py
+++ graphics/lab1/MainWindow.py
@@ -10,22 +10,34 @@ class MainWindow(tk.Frame):
         self.master = master
         # self.pack()
         self.galaxy_type = tk.IntVar()
+        self.slider_value = tk.DoubleVar()
         self.create_widgets()
         self.canvas_center = (300,300)
     
     def create_widgets(self):
-        self.radiobtn_ellipse_galaxy = tk.Radiobutton(self.master,text='Эллиптическая',value = 0,command = self.draw_stars,variable = self.galaxy_type)
+        self.radiobtn_ellipse_galaxy = tk.Radiobutton(self.master,text='Эллиптическая',value = 0,command = self.init_widgets_for_spiral,variable = self.galaxy_type)
         self.radiobtn_ellipse_galaxy.grid(row = 0,column = 0)
- 
+        self.radiobtn_ellipse_galaxy.deselect()
+
         self.radiobtn_mindal_galaxy = tk.Radiobutton(self.master,text='Миндалевидная',value = 1,command = self.draw_stars,variable = self.galaxy_type)
         self.radiobtn_mindal_galaxy.grid(row = 0,column = 1)
+        self.radiobtn_mindal_galaxy.deselect()
  
         self.radiobtn_spiral_galaxy = tk.Radiobutton(self.master,text='Спиральная',value = 2,command = self.draw_stars,variable = self.galaxy_type)
         self.radiobtn_spiral_galaxy.grid(row = 0,column = 2)
+        self.radiobtn_spiral_galaxy.deselect()
+
+        self.show_button = tk.Button(self.master,text = 'Show galaxy')
+        self.show_button.grid(row = 1,column = 0,columnspan = 3)
   
         self.galaxy_area = tk.Canvas(self.master,width = 600 ,height = 600,bg = 'black')
-        self.galaxy_area.grid(row = 1,column = 0,columnspan = 3)
-  
+        self.galaxy_area.grid(row = 4,columnspan = 3)
+
+    
+
+    def init_widgets_for_spiral(self):
+        self.slider = tk.Scale(self.master,variable = self.slider_value,from_=-3,to=3,resolution=0.25)
+        self.slider.grid(row = 2,columnspan = 3)
 
     def calculate_color_of_star(self,distance):
         r = hex((255+distance%255)%255) 
